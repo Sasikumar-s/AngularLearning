2a12b15e559467618dc34bbfee4801ddcff4a0e1
Sasikumar-s <sasikumarbe2018@gmail.com>
2022-02-01 10:31:27 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'heroes';
+  title = 'First Application';
+  name : string="";
+    
+
+  myobj={
+    name : "Sasikumar",
+    email : "sasikumar@gmail.com"
+  };
+
+  myArr =["Sasikumar","Makesh","Aravind","JP"];
+  age:number=0;
+  namelist:Array<string>=[]
+  addElement(){
+    this.namelist.push(this.name);
+  }
+
+  votername:string="";
+  voterage:number=0;
+  voterlist:Array<{votername:string,voterage:number}>=[];
+  addVoter(){
+    this.voterlist.push({votername:this.votername,voterage:this.voterage});
+    this.votername="";
+    this.voterage=0;
+  }
 }
